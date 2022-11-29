4b2dca3b39219ece59afa024cb159e0910fa81a6
Lucas <lcorbi1.lhc@gmail.com>
2022-11-29 15:39:41 -0300

    Added email function

--- MonitorQtdJobs/src/com/macys/domain/Principal.java
+++ MonitorQtdJobs/src/com/macys/domain/Principal.java
@@ -1,12 +1,57 @@
 package com.macys.domain;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import com.macys.dao.RunInfoHistoryDAO;
 
 public class Principal {
+		
+	public static void sendMail(int threshold) throws FileNotFoundException, IOException {
+		
+		try {
+			
+			String fullPathToLogExecution = "MonitorTasksExecution.txt";
+			
+			try {
+				InputStream is = new FileInputStream("list_emails.properties");
+				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+				try (BufferedReader br = new BufferedReader(isr)) {
+					String linemail = "";
+
+					System.out.println("");
+					while ((linemail = br.readLine()) != null) {
+						
+						String smail = "`mail -s " + "\"THERE ARE MORE THAN " + threshold + " tasks running a day! Please, check that! THANK YOU! \" " + linemail + " < " +  fullPathToLogExecution + "`";
+						System.out.println(smail);
+						
+						String[] cmdsendmail = { "/bin/csh", "-c", smail };
+
+						Runtime rtsendmail = Runtime.getRuntime();
+						Process psend = rtsendmail.exec(cmdsendmail);
+						psend.waitFor();
+					}
+				}
+			} catch (FileNotFoundException fne) {
+				System.out.println(
+						"File list_emails.properties not found or not created! Please, create this one by putting the email address line by line. Let this file on the same directory of this .jar file.");
+			}
+
+//		System.out.println(smail);
+
+			System.out.println("");
+			System.out.println("--> Mail sent to Control-M groups!");
+
+		} catch (Exception e) {
+			System.out.println("##### Install the correct mail software!! ####");
+		}
+	}
+			
+	
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
 		
@@ -29,6 +74,26 @@ public class Principal {
 
 		if (quantityOfArguments > 1 || quantityOfArguments == 0) {
 			System.out.println("The program ACCEPTS ONLY 1 argument! Parameter threshold MUST be an INTEGER number, for example: 100000 in which it means 100 thousand of tasks.");
+			System.out.println("");
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("");
+			System.out.println("You MUST create THESE 3 files on the same directory that monitorJobs.jar file is: configs.properties, list_emails.properties and MonitorTasksExecution.txt");
+			System.out.println("");
+			System.out.println(" - On configs.properties, you must add these lines according to: ");
+			System.out.println("");
+					
+			System.out.println("prop.server.host = <IP of Control-EM Server>");
+			System.out.println("prop.server.password = <DB password>");
+			System.out.println("prop.server.user = <DB user>");
+			System.out.println("prop.server.dbname = <DB name>");
+			
+			System.out.println("");
+			System.out.println(" - On list_emails.properties you MUST the emails line by line");
+			System.out.println("");
+			System.out.println(" - On MonitorTasksExecution.txt you should ENTER the message you want to alert via email.");
+			System.out.println("");
+			System.out.println("------------------------------------------------------------------------------------------------------------------------------------------------");
+			System.out.println("");
 		} else {
 			//int total = rdao.showQuantityOfJobsIn24Hours(od);
 			int total = rdao.showQuantityOfJobsIn24Hours(resultprt.replaceAll("\"", "")); //replace takes out the double quotes from String value
@@ -36,9 +101,7 @@ public class Principal {
 			int threshold = 0;
 
 			threshold = Integer.parseInt(args[0]);
-			String linemail = "lucashc@kyndryl.com";
-			String fullPathToLogExecution = "/tmp/MonitorExecuted";
-			Process psend = null;
+						
 		
 			/*System.out.println("");
 			System.out.println("Total: " + total);
@@ -48,19 +111,11 @@ public class Principal {
 			if (total <= threshold) {
 				System.out.println("There are less than " + threshold + " jobs!");
 			} else {
-				System.out.println("CAUTION!! There are more than "  + threshold + " JOBS running daily! Currently running: " + total
-						+ " jobs running in the day!");
-				
-				String smail = "mail -s " + "\"THERE ARE MORE THAN " + threshold + " Please, check that! THANK YOU!" + linemail + " < " +  fullPathToLogExecution;
-				
-				String[] cmdsendmail = { "/bin/sh", "-c", smail };
-				
-				Runtime rtsendmail = Runtime.getRuntime();
-				psend = rtsendmail.exec(cmdsendmail);
-				psend.waitFor();
+				/*System.out.println("CAUTION!! There are more than "  + threshold + " JOBS running daily! Currently running: " + total
+						+ " jobs running in the day!");*/
+				sendMail(threshold);
 				
 			}
-			psend.destroy();
 		}
 		prt.destroy();
 	}
